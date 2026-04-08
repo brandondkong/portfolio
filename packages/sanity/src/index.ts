775320fe0e5c6a7031e102f67a2e6e643d96c175
Brandon Kong <95515854+brandon-kong@users.noreply.github.com>
2026-04-08 13:39:28 -0700

    refactor: update sanity type imports to use @kong/sanity

--- packages/sanity/src/index.ts
+++ packages/sanity/src/index.ts
@@ -3,3 +3,5 @@ import { companyType } from './companyType';
 import { experienceType } from './experienceType';
 
 export const schemaTypes = [workType, companyType, experienceType];
+export { workType, companyType, experienceType };
+export type { Work, WorkFeature, WorkLink, WorkImage } from './workType';
