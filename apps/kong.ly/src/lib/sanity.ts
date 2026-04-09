0f04fbe5af0a53aa683e9c985ea4c48ccdd2a51f
Brandon Kong <95515854+brandon-kong@users.noreply.github.com>
2026-04-09 02:48:36 -0700

    refactor: use createImageUrlBuilder and enable auto format

--- apps/kong.ly/src/lib/sanity.ts
+++ apps/kong.ly/src/lib/sanity.ts
@@ -1,9 +1,9 @@
-import imageUrlBuilder from '@sanity/image-url';
+import { createImageUrlBuilder } from '@sanity/image-url';
 import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
 import { sanityClient } from 'sanity:client';
 
-const builder = imageUrlBuilder(sanityClient);
+const builder = createImageUrlBuilder(sanityClient);
 
 export function urlFor(source: SanityImageSource) {
-    return builder.image(source);
+    return builder.image(source).auto('format');
 }
